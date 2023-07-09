2f040f86f982ccaf015d8492041d4d643d77a07a
AndruSanchez <andrue.sanchezl.98@gmail.com>
2023-07-09 19:12:54 -0400

    checkpoint commit}

--- 03.js
+++ 03.js
@@ -4,17 +4,37 @@
   código dentro de las funciones ya definidas. 
   No comentar la funcion 
 */
-function buscarAmigo(amigos, nombre) {
+function buscarAmigo(amigos, nombre) { // H: amigos =[{nombre:"Alejo" , edad:54},{...},{...}] , nombre= string
+  // T: nombre="Alejo"  --> {nombre:"Alejo" , edad:54}
   // La funcion llamada 'buscarAmigo' recibe como argumento un array llamado 'amigos' que contiene
   // en cada posición del arreglo un objeto que tiene como propiedades 'nombre' y 'edad'. También
   // recibe un string llamado 'nombre'.
   // Debe devolver el objeto cuya propiedad 'nombre' coincida con el string 'nombre' recibido por argumento.
-  // Ej:
+  // Ej: se busca el objeto que coincida con el nombre que nos pasan por parametro
   //  var amigos = [{ nombre: 'toni', edad: 33 } , { nombre: 'Emi', edad: 25 }];
   //  buscarAmigo(amigos, 'toni') debe devolver { nombre: 'toni', edad: 33 };
 
   // Tu código aca:
-  
+  for(i=0 ; i < amigos.length ; i++) {
+    if(amigos[i].nombre === nombre) {
+      return(amigos[i]);
+    }
+  }
+
+  // o 
+
+  /*   
+   var amigoEncontrado = {}
+
+  for(i=0 ; i < amigos.length ; i++) {
+    if(amigos[i].nombre === nombre) {
+      amigoEncontrado = amigos[i]
+    }
+  }
+
+  return(amigosEncontrado);
+    */
+
 }
 
 // No modifiques nada debajo de esta linea //
